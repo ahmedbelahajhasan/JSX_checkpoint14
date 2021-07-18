da7980283a414f0d72c63f81ae1f7bc1a892bb4f
ahmed belhaj hasan <ahmed.belhajhasan@esprit.tn>
2021-07-18 19:56:55 +0100

    first commit

---
+++ mon-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Image1 from "./imageInSrc.jpg"
+import './style.css'
+function App() {
+  return (
+    <div className="background">
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="titlered">JSX</h1>
+      <br />
+      <img className="image1" src={Image1} />
+      <br />
+      <img  src="imageInPublic.jpg" />
+    </div>
+    <video width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </div>
+  );
+}
+
+export default App;
